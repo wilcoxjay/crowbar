9899259f20375bd6dd074f15ad9a1de1f54c2db4
James R. Wilcox <wilcoxjay@gmail.com>
2020-06-13 15:48:41 -0700

    .

--- src/crowbar.ml
+++ src/crowbar.ml
@@ -37,7 +37,15 @@ let fix f =
   unlazy lazygen
 
 let map (type f) (type a) (gens : (f, a) gens) (f : f) =
-  { strategy = Map (gens, f); small_examples = match gens with [] -> [f] | _ -> [] }
+  let rec smalls: type f a. (f, a) gens -> f -> a list = fun gens f ->
+    match gens with
+    | [] -> [f]
+    | g :: gs ->
+       match g.small_examples with
+       | [] -> []
+       | x :: _ -> smalls gs (f x)
+  in 
+  { strategy = Map (gens, f); small_examples = smalls gens f }
 
 let dynamic_bind m f = {strategy = Bind(m, f); small_examples = [] }
 
