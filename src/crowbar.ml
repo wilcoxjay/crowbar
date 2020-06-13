3997a70258587ea33ce8b0c13878c3a1d83380b8
James R. Wilcox <wilcoxjay@gmail.com>
2020-06-13 14:03:21 -0700

    .

--- src/crowbar.ml
+++ src/crowbar.ml
@@ -259,7 +259,7 @@ let rec generate : type a . int -> state -> a gen -> a * unit printer =
   else begin
     if size <= 1 && (match gen.strategy with Unlazy _ -> false | _ -> true) then begin
         print_string "jrw: ";
-        print_endline (stratname gen.strategy)
+        print_endline (stratname gen.strategy);
         failwith "jrw"
       end;
   match gen.strategy with
